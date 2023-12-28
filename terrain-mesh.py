979b8570d51e7bc3a14ffea67d119494800fc924
doronin-nikita <nikitka.doronin@gmail.com>
2023-12-28 10:33:08 +0400

    -

--- terrain-mesh.py
+++ terrain-mesh.py
@@ -2,7 +2,7 @@ import numpy as np
 import pyvista as pv
 from pyvista import examples
 dem = examples.download_crater_topo()
-dem = dem.extract_subset((40,90,40,90,0,0), (1,1,1))
+dem = dem.extract_subset((40,900,40,900,0,0), (1,1,1))
 terrain = dem.warp_by_scalar()
 #terrain.plot()
 
